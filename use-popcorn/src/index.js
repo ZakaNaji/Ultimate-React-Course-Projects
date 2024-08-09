025aef52a11833a8f408c1bce02d445825f7e7c4
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-08-09 17:15:31 +0100

    create a very custumizable star rating component.

--- use-popcorn/src/index.js
+++ use-popcorn/src/index.js
@@ -1,5 +1,18 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+//import "./index.css";
+import StarRating from "./StarRating";
+import { useState } from "react";
+
+const Test = () => {
+  const [rating, setRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} onSetRating={setRating} />
+      <p>YOU DID RATE THIS A : {rating}</p>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Test />);
