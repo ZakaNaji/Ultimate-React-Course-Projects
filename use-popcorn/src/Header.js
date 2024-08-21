724ed5823415352b47a0c68c0a2073d107db3a83
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-08-21 13:51:36 +0100

    use-popcorn: add functionality - on keydown(enter) focuse on search input and empty the query.

--- use-popcorn/src/Header.js
+++ use-popcorn/src/Header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Header({ children }) {
   return <nav className="nav-bar">{children}</nav>;
@@ -14,6 +14,18 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    const callBack = (e) => {
+      if (document.activeElement === inputRef.current) return;
+      if (e.key === "Enter") {
+        inputRef.current.focus();
+        setQuery("");
+      }
+    };
+    document.addEventListener("keydown", callBack);
+    return () => document.removeEventListener("keydown", callBack);
+  }, []);
   return (
     <input
       className="search"
@@ -21,6 +33,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputRef}
     />
   );
 }
