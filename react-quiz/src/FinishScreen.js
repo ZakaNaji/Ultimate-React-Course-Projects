2f566e5a37f7bfc65bec20d765225a6ba919b2d7
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-08-24 20:44:56 +0100

    react-quize: impressions emojis.

--- react-quiz/src/FinishScreen.js
+++ react-quiz/src/FinishScreen.js
@@ -3,9 +3,26 @@ import React from "react";
 export default function FinishScreen({ points, totalAvailablePoints }) {
   return (
     <p className="result">
+      {getImpressions(getPercentage(points, totalAvailablePoints))}
       You have finished the quiz! <br />
       Your score is: {points} out of {totalAvailablePoints} (
-      {Math.round((points / totalAvailablePoints) * 100)}%)
+      {getPercentage(points, totalAvailablePoints)}%)
     </p>
   );
+}
+
+function getPercentage(points, totalAvailablePoints) {
+  return Math.round((points / totalAvailablePoints) * 100);
+}
+
+function getImpressions(percent) {
+  let imoji = "";
+  if (percent < 30) {
+    imoji = "😞";
+  } else if (percent < 70) {
+    imoji = "😐";
+  } else {
+    imoji = "😊";
+  }
+  return imoji;
 }
