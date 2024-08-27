74cbd8d6f8ea7800fb6de44d1168ef79fe711654
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-08-27 10:24:34 +0100

    react-word-map: create diffrent pages/components.

--- react-word-map/src/App.jsx
+++ react-word-map/src/App.jsx
@@ -3,13 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/product";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Pricing from "./pages/Pricing";
+import PageNavigation from "./components/PageNavigation";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <PageNavigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
