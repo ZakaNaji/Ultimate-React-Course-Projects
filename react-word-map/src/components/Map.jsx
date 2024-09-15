f556e1b999cd420684ecfcacabbec6813790127a
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-09-15 13:02:12 +0100

    react-world -map: install and add react-leaflet map component(this is a 3rd party lib)

--- react-word-map/src/components/Map.jsx
+++ react-word-map/src/components/Map.jsx
@@ -1,8 +1,10 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { TileLayer, MapContainer, Marker, Popup } from "react-leaflet";
 
 export default function Map() {
   const navigate = useNavigate();
+  const position = [51.505, -0.09];
 
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
@@ -10,9 +12,22 @@ export default function Map() {
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <p>Latitude: {lat}</p>
-      <p>Longitude: {lng}</p>
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
