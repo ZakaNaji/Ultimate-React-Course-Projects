f5ba9f711dba406a07b39f99b2e971918b31c1a7
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-09-16 14:16:23 +0100

    react-world-map: utility function getData, used in form comp, to get city info after a click on a position on the map.

--- react-word-map/src/components/Form.jsx
+++ react-word-map/src/components/Form.jsx
@@ -1,10 +1,14 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import { useUrlLocation } from "../hooks/useUrlLocation";
+import { getData } from "../utils/utilities";
+
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -19,6 +23,24 @@ function Form() {
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [emoji, setEmoji] = useState("");
+  const [lat, lng] = useUrlLocation();
+
+  useEffect(() => {
+    const url = `${BASE_URL}?latitude=${lat}&longitude=${lng}`;
+    getData(
+      url,
+      (data) => {
+        console.log(data);
+        setCityName(data.city);
+        setCountry(data.countryName);
+        setEmoji(convertToEmoji(data.countryCode));
+      },
+      setIsLoading,
+      null
+    );
+  }, [lat, lng]);
 
   return (
     <form className={styles.form}>
@@ -29,7 +51,7 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
+        {<span className={styles.flag}>{emoji}</span>}
       </div>
 
       <div className={styles.row}>
---
+++ react-word-map/src/utils/utilities.js
@@ -0,0 +1,19 @@
+export function getData(url, setData, setIsLoading, setError) {
+  setIsLoading?.(true);
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setData(data);
+    })
+    .catch((error) => {
+      setError?.(error);
+    })
+    .finally(() => {
+      setIsLoading?.(false);
+    });
+}
