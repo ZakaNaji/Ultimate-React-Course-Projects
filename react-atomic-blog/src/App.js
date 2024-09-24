03daa5f76302cd1650828919fd49c653cd5eac6b
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-09-24 17:55:42 +0100

    react-atomic-blog: we have the same problem if we pass a fucntion as an input.

--- react-atomic-blog/src/App.js
+++ react-atomic-blog/src/App.js
@@ -63,7 +63,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive showAndTitle={archiveInfo} />
+      <Archive showAndTitle={archiveInfo} onAddPost={handleAddPost} />
       <Footer />
     </section>
   );
@@ -160,7 +160,7 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function ({ showAndTitle }) {
+const Archive = memo(function ({ showAndTitle, onAddPost }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
@@ -183,7 +183,7 @@ const Archive = memo(function ({ showAndTitle }) {
               <p>
                 <strong>{post.title}:</strong> {post.body}
               </p>
-              <button>Clicl</button>
+              <button onClick={() => onAddPost(post)}>Clicl</button>
             </li>
           ))}
         </ul>
