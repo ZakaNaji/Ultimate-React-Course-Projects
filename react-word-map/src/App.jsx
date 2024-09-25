4b04117c81c2e1137b89c063573dba58de2ddedd
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-09-25 17:38:55 +0100

    react-world-map: optimization, use lazy loading for each page, lazy() takes a callback that return the dynamic import of the page, and the import will only be called after the page is needed to be rendered for the 1st time. Suspens comp enclose all our pages and take a fullback comp that will be redered untill our imported page is loaded (meaning will have a full page spinner untill the corrispanding page is loaded).

--- react-word-map/src/App.jsx
+++ react-word-map/src/App.jsx
@@ -1,45 +1,54 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import Homepage from "./pages/Homepage";
-import Pricing from "./pages/Pricing";
-import Product from "./pages/Product";
-import Login from "./pages/Login";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { FakeAuthProvider } from "./contexts/FakeAuthContext";
+
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { FakeAuthProvider } from "./contexts/FakeAuthContext";
+import SpinnerFullPage from "./components/SpinnerFullPage";
 import ProtectRoutes from "./pages/ProtectRoutes";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 export default function App() {
   return (
     <CitiesProvider>
       <FakeAuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="product" element={<Product />} />
-            <Route path="login" element={<Login />} />
-            <Route
-              path="app"
-              element={
-                <ProtectRoutes>
-                  <AppLayout />
-                </ProtectRoutes>
-              }
-            >
-              <Route index element={<Navigate to="cities" replace={false} />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="product" element={<Product />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectRoutes>
+                    <AppLayout />
+                  </ProtectRoutes>
+                }
+              >
+                <Route
+                  index
+                  element={<Navigate to="cities" replace={false} />}
+                />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </FakeAuthProvider>
     </CitiesProvider>
