a585f3f113cf78c9526fcb198f244799908a4949
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-09-30 21:21:17 +0100

    redux-intro: add redux-devtools-extension dep to better track and understand redux state/actions management.

--- redux-intro/src/Store.js
+++ redux-intro/src/Store.js
@@ -2,11 +2,15 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import customerReducer from "./features/customers/CustomerSlice";
 import accountReducer from "./features/accounts/AccountSlice";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
