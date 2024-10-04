ed27d6a76c5aa3a3b3c71314d5826b75393eab52
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-04 18:32:09 +0100

    react-fast-pizza: add routes, - add RouterProvider as our app context, it take a router arg. - routes created using: createBrowserRouter([]) - createBrowserRouter takes an array or routes objects: {path:"/", element:<Home/>}

--- react-fast-pizza/src/App.jsx
+++ react-fast-pizza/src/App.jsx
@@ -1,3 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import OrderItem from "./features/order/OrderItem";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <OrderItem /> },
+]);
+
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
