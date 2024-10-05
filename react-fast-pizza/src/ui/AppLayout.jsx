da11777872e4a21c1aa0e1e383c0a0a5fdbc6889
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-05 12:31:26 +0100

    react-fast-pizza: add loading functionality, useNavigation has a status attribute that will be "loading" when the page is bean loaded.

--- react-fast-pizza/src/ui/AppLayout.jsx
+++ react-fast-pizza/src/ui/AppLayout.jsx
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverView from "../features/cart/CartOverview";
+import Loader from "./Loader";
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <Outlet />
