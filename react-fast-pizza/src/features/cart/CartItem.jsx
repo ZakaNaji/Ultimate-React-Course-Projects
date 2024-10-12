bd40df808a689069305d484518f34d5e5c2c1fcb
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-12 19:05:37 +0100

    react-fast-pizza: delete item from cart action.

--- react-fast-pizza/src/features/cart/CartItem.jsx
+++ react-fast-pizza/src/features/cart/CartItem.jsx
@@ -1,8 +1,11 @@
-import Button from '../../ui/Button';
-import { formatCurrency } from '../../utils/helpers';
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
+import { deleteItem } from "./cartSlice";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  const dispatch = useDispatch();
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -11,7 +14,9 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <Button type="small" onClick={() => dispatch(deleteItem(pizzaId))}>
+          Delete
+        </Button>
       </div>
     </li>
   );
