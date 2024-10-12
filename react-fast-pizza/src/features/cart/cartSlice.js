a680af212d82198f833990a172bd56795ea3f31e
ZakaNaji <zakaria.naji@edu.uca.ma>
2024-10-12 18:54:14 +0100

    react-fast-pizza: retrieve cart state from store wiht help of useSelectore.

--- react-fast-pizza/src/features/cart/cartSlice.js
+++ react-fast-pizza/src/features/cart/cartSlice.js
@@ -1,7 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+    {
+      pizzaId: 6,
+      name: "Vegetale",
+      quantity: 1,
+      unitPrice: 13,
+      totalPrice: 13,
+    },
+    {
+      pizzaId: 11,
+      name: "Spinach and Mushroom",
+      quantity: 1,
+      unitPrice: 15,
+      totalPrice: 15,
+    },
+  ],
 };
 
 const cartSlice = createSlice({
@@ -12,15 +34,15 @@ const cartSlice = createSlice({
       state.cart.push(action.payload);
     },
     deleteItem(state, action) {
-      state.cart = state.cart.filter((item) => item.id !== action.payload);
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
     },
     increaseQuantity(state, action) {
-      let item = state.cart.find((i) => i.id === action.payload);
+      let item = state.cart.find((i) => i.pizzaId === action.payload);
       item.quantity++;
       item.totalPrice = item.unitPrice * item.quantity;
     },
     decreaseQuantity(state, action) {
-      let item = state.cart.find((i) => i.id === action.payload);
+      let item = state.cart.find((i) => i.pizzaId === action.payload);
       item.quantity--;
       item.totalPrice = item.unitPrice * item.quantity;
     },
--- react-fast-pizza/src/features/menu/MenuItem.jsx
+++ react-fast-pizza/src/features/menu/MenuItem.jsx
@@ -1,20 +1,22 @@
-import Button from '../../ui/Button';
-import { formatCurrency } from '../../utils/helpers';
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const dispatch = useDispatch();
 
   return (
     <li className="flex gap-4 py-2">
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+        className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
       />
       <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
-          {ingredients.join(', ')}
+          {ingredients.join(", ")}
         </p>
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
